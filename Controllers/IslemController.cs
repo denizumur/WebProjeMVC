2b358cb10d252c0176f4ab7a7f8f52bb8fdb92a3
denizumur <deniz.umur@ogr.sakarya.edu.tr>
2024-12-17 23:23:34 +0300

    EKLEME-SİLME-GÜNCELLEME İşlemleri

---
+++ Controllers/IslemController.cs
@@ -0,0 +1,53 @@
+﻿using berber.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace berber.Controllers
+{
+    public class IslemController : Controller
+    {
+        Context c=new Context();
+        public IActionResult Index()
+        {
+            var degerler=c.Islemler.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public IActionResult YeniIslem()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult YeniIslem(Islem islem)
+        {
+            c.Islemler.Add(islem);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+            
+
+            
+        }
+        public IActionResult IslemSil(int id)
+        {
+            var islem=c.Islemler.Find(id); 
+            c.Islemler.Remove(islem);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+
+            return View();
+        }
+        public IActionResult IslemGetir(int id)
+        {
+            var islem = c.Islemler.Find(id);
+            return View("IslemGetir", islem);
+        }
+        public IActionResult IslemGuncelle(Islem i)
+        {
+            var islem = c.Islemler.Find(i.IslemID);
+            islem.Ucret = i.Ucret;
+            islem.IslemAdi = i.IslemAdi;
+            islem.Sure = i.Sure;
+            c.SaveChanges();
+            return View("Index");
+        }
+    }
+}
--- obj/Debug/net8.0/berber.AssemblyInfo.cs
+++ obj/Debug/net8.0/berber.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("berber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+541929b4619399324b8d83908dba46ab4c7dfcd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fc0908162bbe0bb5bcd9bf0e7c4b1089a2a59c")]
 [assembly: System.Reflection.AssemblyProductAttribute("berber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("berber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
