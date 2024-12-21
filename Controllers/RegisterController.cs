532732bd7cd27bd13144e7fa33ac40be9dbb93d4
melihdedeoglu <melihdedeoglu0@gmail.com>
2024-12-21 21:39:56 +0300

    Admin paneli düzenlemesi ve register düzenlemesi
    Admin paneli, register ve diğer değişiklikler yapıldı.

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,20 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using berber.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace berber.Controllers
 {
 	public class RegisterController : Controller
 	{
+		Context c = new Context();
 		public IActionResult Kayit()
 		{
 
 
 			return View();
 		}
-		public IActionResult KullaniciEkle()
+		[HttpPost]
+		public IActionResult KullaniciEkle(Kullanici user)
 		{
+			if (ModelState.IsValid)
+			{
+				// Kullanıcı bilgilerini veritabanına ekle
+				user.UyelikTarihi = DateTime.Now;
+				c.Kullanicilar.Add(user);
+				c.SaveChanges();
 
+				return RedirectToAction("KullaniciGoruntuleme","Admin"); // Başarılı bir yönlendirme
+			}
 
-			return View();
+			var errors = ModelState.Values.SelectMany(v => v.Errors);
+			foreach (var error in errors)
+			{
+				Console.WriteLine(error.ErrorMessage); // Hataları kontrol edin
+			}
+
+			// Hatalı ise aynı formu tekrar göster
+			return View("Kayit", user); // Form sayfasını tekrar yükler
 		}
 	}
 }
