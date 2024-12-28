390a3ea06a952f62bdaa323cc8d2072eaa86d420
melihdedeoglu <melihdedeoglu0@gmail.com>
2024-12-28 17:27:36 +0300

    değişiklikler eklendi

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', () => {
+    // Menü açma ve kapama fonksiyonu
+    const burger = document.querySelector('.burger');
+    burger.addEventListener('click', () => {
+        document.body.classList.toggle('open');
+    });
 
-// Write your JavaScript code.
+    // Dropdown menüler için açma ve kapama işlemleri
+    const dropdownButtons = document.querySelectorAll('.dropdown .button');
+    dropdownButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            const dropdownMenu = button.nextElementSibling;
+            dropdownMenu.classList.toggle('open');
+            event.stopPropagation();
+        });
+    });
+
+    // Sayfa dışında bir yere tıklanınca dropdown menüyü kapatma
+    document.addEventListener('click', () => {
+        const openDropdowns = document.querySelectorAll('.dropdown-menu.open');
+        openDropdowns.forEach(menu => {
+            menu.classList.remove('open');
+        });
+    });
+});
